7745985ccb144ad9bc3f515b45f5cfc4d1f03b86
tosini <marco.tosini@bitrock.com>
2019-06-25 16:42:22 +0200

    EitherTValues for testing

--- src/main/scala/com/frames/cassandra/Config.scala
+++ src/main/scala/com/frames/cassandra/Config.scala
@@ -5,7 +5,7 @@ import com.typesafe.config.ConfigFactory
 object Config {
   private val config = ConfigFactory.load()
 
-  lazy val CassandraHost = config.getString("cassandra.host").split(",")
-  lazy val CassandraPort = config.getInt("cassandra.port")
+  lazy val CassandraHost       = config.getString("cassandra.host").split(",")
+  lazy val CassandraPort       = config.getInt("cassandra.port")
   lazy val DefaultScriptFolder = config.getString("cassandra.scriptFolder")
 }

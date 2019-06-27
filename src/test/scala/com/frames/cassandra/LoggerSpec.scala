201acbccfac87cf7a353100bcae76bdebd896fd7
tosini <marco.tosini@bitrock.com>
2019-06-27 15:40:45 +0200

    Logger Spec

---
+++ src/test/scala/com/frames/cassandra/LoggerSpec.scala
@@ -0,0 +1,13 @@
+package com.frames.cassandra
+
+import org.scalatest.{Matchers, WordSpec}
+
+class LoggerSpec extends WordSpec with Matchers {
+
+  "Logger" should {
+
+    "log messages" in {}
+
+  }
+
+}
